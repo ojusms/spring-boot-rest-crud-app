4c8c25939dedd26489b3c7f38130d0593cac7947
Ojus M S <ojusms@gmail.com>
2026-06-06 23:21:23 -0700

    Initial commit - Spring Boot project from Spring Initializr

---
+++ src/test/java/com/SpringBoot/RestCrudApp/RestCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.RestCrudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
