9e525969e5c072c8868513315516d08edf94dfad
Ojus M S <ojusms@gmail.com>
2026-06-07 02:52:39 -0700

    Update EmployeeRestController to expose a POST endpoint to add a new employee

--- src/main/java/com/SpringBoot/RestCrudApp/REST/EmployeeRestController.java
+++ src/main/java/com/SpringBoot/RestCrudApp/REST/EmployeeRestController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,8 +45,23 @@ public class EmployeeRestController {
 			throw new RuntimeException("Employee ID not found - " + employeeId);
 		
 		// return employee
-		return theEmployee;
-		
+		return theEmployee;		
 	}
 
+	// expose an endpoint for POST "/employees" to add a new employee. @RequestBody tells the controller that the HTTP
+	// request body should be bound to the method parameter which is theEmployee here
+	
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		
+		// set the employee ID to 0 in case the user has included one
+		theEmployee.setId(0);
+		
+		// save the employee
+		Employee dbEmployee = employeeService.save(theEmployee);
+		
+		// return the saved employee
+		return dbEmployee;
+	}
+	
 }
