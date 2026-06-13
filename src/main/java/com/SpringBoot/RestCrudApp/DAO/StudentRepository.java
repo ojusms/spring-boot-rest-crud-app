5c6a702952790d12b620a71cf946384d8994335e
Ojus M S <ojusms@gmail.com>
2026-06-12 22:02:04 -0700

    Commenting out Spring Data REST dependency in Maven POM and related code in StudentRepository to test Open API documentation due to a known issue with SpringDoc not working properly when this is present

--- src/main/java/com/SpringBoot/RestCrudApp/DAO/StudentRepository.java
+++ src/main/java/com/SpringBoot/RestCrudApp/DAO/StudentRepository.java
@@ -1,13 +1,16 @@
-package com.SpringBoot.RestCrudApp.DAO;
+//package com.SpringBoot.RestCrudApp.DAO;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//
+//import com.SpringBoot.RestCrudApp.Entity.Student;
+//
+//// @RepositoryRestResource allows the endpoint exposed to be modified. /members instead of /students in this case.
+//
+////@RepositoryRestResource(path = "members")
+//public interface StudentRepository  extends JpaRepository<Student, Integer>{
+//
+//}
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import com.SpringBoot.RestCrudApp.Entity.Student;
-
-// @RepositoryRestResource allows the endpoint exposed to be modified. /members instead of /students in this case.
-
-@RepositoryRestResource(path = "members")
-public interface StudentRepository  extends JpaRepository<Student, Integer>{
-
-}
+// SpringDoc does not work properly with Spring Data REST dependency in the same project. Commenting this code out to
+// test OpenAPI / Swagger documentation
