1a40e4e78d2ff410ac7f50aedb7ed36ab4d64c41
Ojus M S <ojusms@gmail.com>
2026-06-13 01:06:57 -0700

    Update SecurityConfig class to implement Role Based Access Control via HttpSecurity and SecurityFilterChain

--- src/main/java/com/SpringBoot/RestCrudApp/Security/SecurityConfig.java
+++ src/main/java/com/SpringBoot/RestCrudApp/Security/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.SpringBoot.RestCrudApp.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 // Spring Security Configuration class with users having roles which overrides application.properties configs
@@ -34,6 +38,30 @@ public class SecurityConfig {
 				.build();
 		
 		return new InMemoryUserDetailsManager(john, mary, susan);
+	}
+	
+	// add security filter to implement Role Based Access Control for API end points
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(configurer ->
+		configurer
+		.requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+		.requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+		.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+		.requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+		.requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+		);
+		
+		// use HTTP Basic authentication
+		http.httpBasic(Customizer.withDefaults());
+		
+		// disable Cross Site Request Forgery (CSRF)
+		// generally not needed for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+		http.csrf(csrf -> csrf.disable());
+		
+		return http.build();
 	}
 
 }
