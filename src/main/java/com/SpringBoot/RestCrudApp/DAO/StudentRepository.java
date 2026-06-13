b023bd097b31ae7e88617a0eb06fa10ba81a1bf8
Ojus M S <ojusms@gmail.com>
2026-06-12 20:57:15 -0700

    Update application.properties to set default page size for Spring Data REST endpoints results, update StudentRepository interface to use RepositoryRestResource annotation for custom endpoint text

--- src/main/java/com/SpringBoot/RestCrudApp/DAO/StudentRepository.java
+++ src/main/java/com/SpringBoot/RestCrudApp/DAO/StudentRepository.java
@@ -1,9 +1,13 @@
 package com.SpringBoot.RestCrudApp.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.SpringBoot.RestCrudApp.Entity.Student;
 
+// @RepositoryRestResource allows the endpoint exposed to be modified. /members instead of /students in this case.
+
+@RepositoryRestResource(path = "members")
 public interface StudentRepository  extends JpaRepository<Student, Integer>{
 
 }
