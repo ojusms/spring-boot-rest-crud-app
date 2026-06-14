d1c676b3a5d7c8011167addf9ef4fdd306f3d093
Ojus M S <ojusms@gmail.com>
2026-06-13 23:33:39 -0700

    Update SecurityConfig class to add support for JDBC to read user roles from the database

--- src/main/java/com/SpringBoot/RestCrudApp/Security/SecurityConfig.java
+++ src/main/java/com/SpringBoot/RestCrudApp/Security/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.SpringBoot.RestCrudApp.Security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -15,29 +19,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+	// add support for JDBC. No need for hard coded user values, it reads from the DB
 	
 	@Bean
-	public InMemoryUserDetailsManager userDetailsManager( ) {
-		
-		UserDetails john = User.builder()
-				.username("john")
-				.password("{noop}test123") // noop - no operation, stores as plain text as opposed to encrypted
-				.roles("EMPLOYEE")
-				.build();
-		
-		UserDetails mary = User.builder()
-				.username("mary")
-				.password("{noop}test123")
-				.roles("EMPLOYEE","MANAGER")
-				.build();
-		
-		UserDetails susan = User.builder()
-				.username("susan")
-				.password("{noop}test123")
-				.roles("EMPLOYEE","MANAGER","ADMIN")
-				.build();
-		
-		return new InMemoryUserDetailsManager(john, mary, susan);
+	public UserDetailsManager userDetailsManager(DataSource datasource) {
+		return new JdbcUserDetailsManager(datasource);
 	}
 	
 	// add security filter to implement Role Based Access Control for API end points
@@ -65,4 +52,30 @@ public class SecurityConfig {
 		return http.build();
 	}
 
+	/* // previous version of creating hard coded user roles 
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager( ) {
+		
+		UserDetails john = User.builder()
+				.username("john")
+				.password("{noop}test123") // noop - no operation, stores as plain text as opposed to encrypted
+				.roles("EMPLOYEE")
+				.build();
+		
+		UserDetails mary = User.builder()
+				.username("mary")
+				.password("{noop}test123")
+				.roles("EMPLOYEE","MANAGER")
+				.build();
+		
+		UserDetails susan = User.builder()
+				.username("susan")
+				.password("{noop}test123")
+				.roles("EMPLOYEE","MANAGER","ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(john, mary, susan);
+	}
+	*/
+	
 }
